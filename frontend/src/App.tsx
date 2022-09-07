f44008db9c31c0ebdd71bd588bd2ae7609893e5f
Mackcoel <maxtrunder@hotmail.com>
2022-09-07 00:11:51 -0300

    first component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import NotificationButton from "./components/NotificationButton"
+
 
 function App() {
-  return(
-    <h1>olá mundo!</h1>
+  return (
+    <>
+      <h1>olá mundo!</h1>
+      <NotificationButton/>
+    </>
   )
 }
-  
+
 
 export default App
