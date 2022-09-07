e10c2a3dfde6e982d2c32942daae9d1e2872b67f
Mackcoel <maxtrunder@hotmail.com>
2022-09-07 01:09:22 -0300

    Other components

---
+++ frontend/src/components/SalesCard/index.tsx
@@ -0,0 +1,90 @@
+
+import NotificationButton from '../NotificationButton'
+import './styles.css'
+
+function SalesCard() {
+    return (
+        <div className="dsmeta-card">
+            <h2 className="dsmeta-sales-title">Vendas</h2>
+            <div>
+                <div className="dsmeta-form-control-conteiner">
+                    <input className="dsmeta-form-control" type="text" />
+                </div>
+                <div className="dsmeta-form-control-conteiner">
+                    <input className="dsmeta-form-control" type="text" />
+                </div>
+            </div>
+
+            <div>
+                <table className="dsmeta-sales-table">
+                    <thead>
+                        <tr>
+                            <th className="show992">ID</th>
+                            <th className="show576">Data</th>
+                            <th>Vendedor</th>
+                            <th className="show992">Visitas</th>
+                            <th className="show992">Vendas</th>
+                            <th>Total</th>
+                            <th>Notificar</th>
+                        </tr>
+
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576"> 05/09/2022</td>
+                            <th>Anakin</th>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <th>55300.00</th>
+                            <th>
+
+
+                                <div className="dsmeta-red-btn-conteiner">
+                                    <NotificationButton />
+                                </div>
+
+                            </th>
+                        </tr>
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576"> 05/09/2022</td>
+                            <td>Anakin</td>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <th>55300.00</th>
+                            <th>
+
+
+                                <div className="dsmeta-red-btn-conteiner">
+                                <NotificationButton />
+                                </div>
+
+                            </th>
+                        </tr>
+                        <tr>
+                            <td className="show992">#341</td>
+                            <td className="show576"> 05/09/2022</td>
+                            <th>Anakin</th>
+                            <td className="show992">15</td>
+                            <td className="show992">11</td>
+                            <th>55300.00</th>
+                            <th>
+
+
+                                <div className="dsmeta-red-btn-conteiner">
+                                <NotificationButton />
+                                </div>
+
+                            </th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    )
+}
+
+
+export default SalesCard
